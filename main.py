6e3d08c4cbb8761c92cbc0e90752b3783f1539b9
YogeshYadavShorthillsAI <yogesh.yadav@shorthills.ai>
2025-04-22 18:53:48 +0530

    changed commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharmcfdre')
+    print_hi('PyCharmcf')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
